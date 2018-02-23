60c93f162788391e2582a797c1d2bca2aba3503c
Chun-Min Chang <twcmchang@gmail.com>
2018-02-23 12:57:44 +0800

    bash quick_start.py <GPU_ID>

--- quick_start.sh
+++ quick_start.sh
@@ -7,7 +7,7 @@
 ## 1. train with sparsity and monotonicity constraints, prune network, and save a sparse parameter dictionary in save_dir
 ## 2. aggregate losses at different fidelity levels and then fine-tune the pruned model
 ## 3. evaluation the performance of different fidelity levels
-#wget -O save/vgg16.npy "https://www.dropbox.com/s/6u5jp5wzlwajxu1/vgg16.npy?dl=0"
+wget -O save/vgg16.npy "https://www.dropbox.com/s/6u5jp5wzlwajxu1/vgg16.npy?dl=0"
 CUDA_VISIBLE_DEVICES=$1 python3 train.py --prof_type linear --lambda_s 0.001 --lambda_m 0.001 --decay 0.00005 --keep_prob 1 --save_dir save/ --init_from save/vgg16.npy
 CUDA_VISIBLE_DEVICES=$1 python3 train.py --tesla 1 --decay 0.00005 --keep_prob 1 --init_from save/sparse_dict.npy --save_dir save/
 CUDA_VISIBLE_DEVICES=$1 python3 test.py --keep_prob 1 --init_from save/finetune_dict.npy --output save/output.csv
